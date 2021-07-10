da6bd445efe5012df4b5e507188ce36ea595e05a
Ajinkya Ghodekar <ajinkyasghodekar@gmail.com>
2021-07-10 21:24:23 +0530

    updated scheduled timing by using SimpleDateFormat and Date api's in java.

--- src/main/java/in/practice/ajinkya/SpringBootSchedulingBasicsApplication.java
+++ src/main/java/in/practice/ajinkya/SpringBootSchedulingBasicsApplication.java
@@ -11,5 +11,4 @@ public class SpringBootSchedulingBasicsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSchedulingBasicsApplication.class, args);
 	}
-
 }
--- src/main/java/in/practice/ajinkya/schedule/ScheduleEx.java
+++ src/main/java/in/practice/ajinkya/schedule/ScheduleEx.java
@@ -9,9 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleEx {
 	SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
-	Date d = new Date();
-	@Scheduled(initialDelay = 5000, fixedDelay = 5000)
+	@Scheduled(initialDelay = 3000, fixedDelay = 5000)
 	public void m1() {
-		System.out.println("Hello: ");
+		System.out.println("Hello: "+sdf.format(new Date()));
+	}
+	@Scheduled(initialDelay = 2000, fixedRate = 4000)
+	public void m2() {
+		System.out.println("Word: "+sdf.format(new Date()));
 	}
 }
